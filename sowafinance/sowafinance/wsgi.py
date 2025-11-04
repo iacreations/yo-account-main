1a060e48f9c1ecf105117d359d33604afe64b582
iacreations <iacreations256@gmail.com>
2025-11-04 16:15:01 +0300

    wsgi fixing

--- sowafinance/sowafinance/wsgi.py
+++ sowafinance/sowafinance/wsgi.py
@@ -6,6 +6,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # ✅ Use the inner package ONLY (no double prefix)
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sowafinance.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sowafinance.sowafinance.settings')
 
 application = get_wsgi_application()
