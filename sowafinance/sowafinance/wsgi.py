0e1a54e7fa31debe975b368a9c6c11c2ddab5c77
iacreations <iacreations256@gmail.com>
2025-11-04 15:44:49 +0300

    deploy changes

--- sowafinance/sowafinance/wsgi.py
+++ sowafinance/sowafinance/wsgi.py
@@ -1,16 +1,11 @@
 """
 WSGI config for sowafinance project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sowafinance.sowafinance.settings')
+# ✅ Use the inner package ONLY (no double prefix)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sowafinance.settings")
 
 application = get_wsgi_application()
