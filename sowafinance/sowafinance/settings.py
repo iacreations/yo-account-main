c44b8d347c5471e0b1af1d22b5439dc6ea78fbff
iacreations <iacreations256@gmail.com>
2025-11-04 13:48:52 +0300

    settings update

--- sowafinance/sowafinance/settings.py
+++ sowafinance/sowafinance/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = os.environ.get("DEBUG", "False").lower()="true"
+DEBUG = os.environ.get("DEBUG", "False").lower()=="true"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 # end of environ variables
